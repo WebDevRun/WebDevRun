e34a8a23b0b50b704b3e1bbba06400b595be3abd
Karamashev Leonid <leonkaram@mail.ru>
2022-10-29 00:32:51 +0700

    update fileParser

--- filesDriver/fileParser.js
+++ filesDriver/fileParser.js
@@ -1,38 +1,88 @@
 const requisitesComparisons = require('../helpers/requisitesComparisons')
 class FileParser {
-  #splitValue = ';'
+  #space = ' '
+  #semicolon = ';'
   #emptyString = ''
+  #doubleQuotes = '"'
+  #invalidDate = 'Invalid Date'
+  #returnSymbol = '\r'
+  #newlineSymbol = '\n'
   #english = {
     name: 'английский',
     code: 'п:09; у:29',
+    written: 'п',
+    oral: 'у',
   }
 
   constructor(data) {
     this.data = data
   }
 
-  #normalize(string) {
+  #getExcludeComparisons() {
+    return requisitesComparisons.filter((comparison) => {
+      return comparison?.exclude === true
+    })
+  }
+
+  #getRequiredComparisons() {
+    return requisitesComparisons.filter((comparison) => {
+      const isAllowNull = comparison?.allowNull === false
+      const isUnique = comparison?.unique === true
+      return isAllowNull || isUnique || (isAllowNull && isUnique)
+    })
+  }
+
+  #trimLeft(string) {
+    while (string.startsWith(this.#semicolon) && string.length) {
+      string = string.slice(1)
+    }
+
+    return string
+  }
+
+  #normalizeTitle(string) {
     let normalizeString = string.trim().toLowerCase()
-    if (normalizeString.startsWith('"'))
+    if (normalizeString.startsWith(this.#doubleQuotes))
       normalizeString = normalizeString.slice(1)
-    if (normalizeString.endsWith('"'))
+    if (normalizeString.endsWith(this.#doubleQuotes))
       normalizeString = normalizeString.slice(-1)
+
+    normalizeString = this.#trimLeft(normalizeString)
+
     return normalizeString
   }
 
-  #toLocaleDateString(title, index) {
-    return new Date(title[index]).toLocaleDateString()
+  #toLocaleDateString(string) {
+    return new Date(string).toLocaleDateString()
+  }
+
+  #selectDates(array) {
+    let firstDate, secondDate
+
+    for (const item of array) {
+      const date = this.#toLocaleDateString(item)
+      if (date === this.#invalidDate) continue
+      if (firstDate) {
+        secondDate = date
+        continue
+      }
+      firstDate = date
+    }
+
+    return array[2].startsWith(this.#english.written)
+      ? `${this.#english.written}:${firstDate}; ${
+          this.#english.oral
+        }:${secondDate}`
+      : `${this.#english.written}:${secondDate}; ${
+          this.#english.oral
+        }:${firstDate}`
   }
 
   #parseTitle(title) {
-    const splitTitle = this.#normalize(title).split(' ')
+    const splitTitle = this.#normalizeTitle(title).split(this.#space)
 
     if (splitTitle.includes(this.#english.name)) {
-      const firstDate = this.#toLocaleDateString(splitTitle, 2)
-      const secondDate = this.#toLocaleDateString(splitTitle, 3)
-      const date = splitTitle[2].startsWith('п')
-        ? `п:${firstDate}; у:${secondDate}`
-        : `п:${secondDate}; у:${firstDate}`
+      const date = this.#selectDates(splitTitle)
 
       return {
         code: this.#english.code,
@@ -49,64 +99,97 @@ class FileParser {
   }
 
   #getTableHead(tableHead) {
+    const excludeComparisons = this.#getExcludeComparisons()
     const deleteIndexes = []
-    const head = this.#normalize(tableHead)
-      .split(this.#splitValue)
+    let comparisonsCount = 0
+
+    const head = this.#normalizeTitle(tableHead)
+      .split(this.#semicolon)
+      .map((item) => {
+        for (const requisitesComparison of requisitesComparisons) {
+          if (requisitesComparison.values.includes(item)) {
+            comparisonsCount += 1
+            return requisitesComparison?.composite || requisitesComparison.name
+          }
+        }
+        return this.#emptyString
+      })
+      .flat()
       .filter((item, index) => {
-        const emptyStirng = item === this.#emptyString
-        const isPasswort = requisitesComparisons.passport.includes(item)
-        const isNumber = requisitesComparisons.number.includes(item)
-        if (emptyStirng || isNumber || isPasswort) {
+        if (item === this.#emptyString) {
           deleteIndexes.push(index)
           return false
         }
+
+        for (const excludeComparison of excludeComparisons) {
+          if (excludeComparison.name === item) {
+            deleteIndexes.push(index)
+            return false
+          }
+        }
+
         return true
       })
-      .map((item) => {
-        for (const key in requisitesComparisons) {
-          if (requisitesComparisons[key].includes(item)) return key
-        }
-        return item
-      })
+
+    if (!comparisonsCount) throw new Error("don't have head in table")
     return { head, deleteIndexes }
   }
 
   #getTableBoby(tableBody, tableHead) {
     const { head, deleteIndexes } = tableHead
-    const headLength = head.length
     const tableBodyValues = []
 
     for (const row of tableBody) {
       const splitRow = row
         .trim()
-        .split(this.#splitValue)
-        .filter((item, index) => {
-          if (item !== this.#emptyString && !deleteIndexes.includes(index))
-            return true
-          return false
-        })
+        .split(this.#semicolon)
+        .filter((item, index) => !deleteIndexes.includes(index))
         .reduce((acc, item, index) => {
+          if (item === this.#emptyString) return acc
           const key = head[index]
           acc[key] = item
           return acc
         }, {})
 
-      if (Object.keys(splitRow).length === headLength)
-        tableBodyValues.push(splitRow)
+      const splitRowKeys = Object.keys(splitRow)
+      const requiredComparisons = this.#getRequiredComparisons()
+      const difference = requiredComparisons.filter(
+        (comparison) => !splitRowKeys.includes(comparison.name)
+      )
+
+      if (!difference.length) tableBodyValues.push(splitRow)
     }
 
     return tableBodyValues
   }
 
   parseCsv() {
-    const splitedData = this.data.includes('\r\n')
-      ? this.data.split('\r\n')
-      : this.data.split('\n')
+    const splitedData = this.data.includes(
+      `${this.#returnSymbol}${this.#newlineSymbol}`
+    )
+      ? this.data.split(`${this.#returnSymbol}${this.#newlineSymbol}`)
+      : this.data.split(this.#newlineSymbol)
     const [title, tableHead, ...tableBody] = splitedData
     const parsedTitle = this.#parseTitle(title)
-    const parsedHead = this.#getTableHead(tableHead)
+
+    let isRecheking = false
+    let parsedHead
+
+    for (const requisitesComparison of requisitesComparisons) {
+      isRecheking = tableHead
+        .toLowerCase()
+        .includes(requisitesComparison.values)
+    }
+
+    if (isRecheking) {
+      const concatTableHead = tableHead.concat(tableBody.shift())
+      parsedHead = this.#getTableHead(concatTableHead)
+    } else {
+      parsedHead = this.#getTableHead(tableHead)
+    }
+
     const parsedTableBody = this.#getTableBoby(tableBody, parsedHead)
-    return { title: parsedTitle, body: parsedTableBody }
+    return { isRecheking, title: parsedTitle, body: parsedTableBody }
   }
 }
 
--- tests/fileParser.test.js
+++ tests/fileParser.test.js
@@ -10,6 +10,7 @@ describe('FileParser.parseCsv', () => {
       Всего участников;;;;;;;;;;;;;;;;;;;;;;;1;;;;
     `
     const result = {
+      isRecheking: false,
       title: {
         code: '01',
         examName: 'русский язык',
@@ -37,7 +38,7 @@ describe('FileParser.parseCsv', () => {
     expect(fileParser.parseCsv()).toEqual(result)
   })
 
-  test('Should return parsed data when english exam', () => {
+  test('Should return parsed data when english exam 2022', () => {
     const eng = `"Английский язык п:2022.06.14; у:2022.06.16";;;;;;;;;;;;;;;;;;;;;
     №;Фамилия;;;Имя;;;Отчество;;Документ;Код ОО;Класс;Код ППЭ;Аудитория;;Часть с кратким ответом;Часть с развёрнутым ответом;Устная часть;Первичный балл устной части;Первичный балл письм. части;Первичный балл;Тестовый балл
     1;Иванов;;;Иван;;;Иванович;;1111 222222;111001;11Б;п:1111 у:1081;п:0003 у:0002;;+++73---;2(2)2(2);0(1)0(4)0(5);11;45;56;56
@@ -45,6 +46,7 @@ describe('FileParser.parseCsv', () => {
     3;Смирнов;;;Семен;;;Викторович диме;;5555 666666;111002;11;п:1111 у:1081;п:0003 у:0002;;46--++96;1(2)1(2);0(1)4(4)5(5);1;38;39;39
     `
     const result = {
+      isRecheking: false,
       title: {
         code: 'п:09; у:29',
         examName: 'английский язык',
@@ -106,6 +108,44 @@ describe('FileParser.parseCsv', () => {
     expect(fileParser.parseCsv()).toEqual(result)
   })
 
+  test('Should return parsed data when english exam 2021', () => {
+    const eng = `Английский язык п.: 2021.06.18, у.: 2021.06.21;;;;;;;;;;;;;;;;
+    №;Код МСУ;Код ОО;Класс;Код ППЭ;Аудитория;Фамилия;Имя;Отчество;Документ;Часть с кратким ответом;Часть с развёрнутым ответом;Устная часть;Первичный балл устной части;Первичный балл письм. части;Первичный балл;Тестовый балл
+    1;111;111001;11Б;1111;3;Иванов;Иван;Иванович;1111 111111;56+------11--+--++--+----++-++--+----+;1(2)1(2)0(2)0(3)0(3)0(3)0(3)0(2);0(1)1(5)1(3)1(2)0(2)0(3)0(2)0(2);3;26;29;29
+    
+    `
+    const result = {
+      isRecheking: false,
+      title: {
+        code: 'п:09; у:29',
+        examName: 'английский язык',
+        date: 'п:18.06.2021; у:21.06.2021',
+      },
+      body: [
+        {
+          local_government_code: '111',
+          organization_code: '111001',
+          class: '11Б',
+          code_exam_point: '1111',
+          auditorium_number: '3',
+          last_name: 'Иванов',
+          first_name: 'Иван',
+          patronymic: 'Иванович',
+          short_answer: '56+------11--+--++--+----++-++--+----+',
+          detailed_answer: '1(2)1(2)0(2)0(3)0(3)0(3)0(3)0(2)',
+          oral_answer: '0(1)1(5)1(3)1(2)0(2)0(3)0(2)0(2)',
+          oral_score: '3',
+          written_score: '26',
+          prymary_score: '29',
+          estimation: '29',
+        },
+      ],
+    }
+
+    const fileParser = new FileParser(eng)
+    expect(fileParser.parseCsv()).toEqual(result)
+  })
+
   test('Should return parsed data when basic math exam', () => {
     const basicMath = `22 - Математика базовая 2022.06.27;;;;;;;;;;;;
     №;Код ОО;Класс;Код ППЭ;Аудитория;Фамилия;Имя;Отчество;Серия;Номер;Задания с кратким ответом;Первичный балл;Оценка
@@ -116,6 +156,7 @@ describe('FileParser.parseCsv', () => {
     Всего участников;;;;;;;;;;;11;
     `
     const result = {
+      isRecheking: false,
       title: {
         code: '22',
         examName: 'математика базовая',
@@ -163,5 +204,118 @@ describe('FileParser.parseCsv', () => {
 
     const fileParser = new FileParser(basicMath)
     expect(fileParser.parseCsv()).toEqual(result)
+  })
+
+  test('Should return data when informatic exam 2021', () => {
+    const informatics = `25 - Информатика и ИКТ (КЕГЭ) 2021.06.24;;;;;;;;;;;;;;
+    №;Код МСУ;Код ОО;Класс;Код ППЭ;Аудитория;Фамилия;Имя;Отчество;Серия;Номер;Задания с кратким ответом;Задания с развёрнутым ответом;Первичный балл;Балл
+    1;111;111001;11А;1111;2;Иванов;Иван;Иванович;1111;111111;++-+++++++-----+++-++++-000;;16;65
+    `
+    const result = {
+      isRecheking: false,
+      title: {
+        code: '25',
+        examName: 'информатика и икт (кегэ)',
+        date: '24.06.2021',
+      },
+      body: [
+        {
+          local_government_code: '111',
+          organization_code: '111001',
+          class: '11А',
+          code_exam_point: '1111',
+          auditorium_number: '2',
+          last_name: 'Иванов',
+          first_name: 'Иван',
+          patronymic: 'Иванович',
+          short_answer: '++-+++++++-----+++-++++-000',
+          prymary_score: '16',
+          estimation: '65',
+        },
+      ],
+    }
+
+    const fileParser = new FileParser(informatics)
+    expect(fileParser.parseCsv()).toEqual(result)
+  })
+
+  test('Should return normalized head', () => {
+    const head = `;;;;;;;12 - Обществознание 2021.06.15;;;;;;;;;;;
+    №;;Код МСУ;;Код ОО;;Класс;;Код ППЭ;;Аудитория;;Фамилия;Имя;Отчество;;Серия;;Номер;;Задания с кратким ответом;;Задания с развёрнутым ответом;;Первичный балл;;Балл;
+    1;;111;;111002;;11;;1018;;9;;Иванов;Иван;Иванович;;1111;;222222;;++++++;;1(1)1(1)1(1)1(1);;6;;39;
+    `
+
+    const result = {
+      isRecheking: false,
+      title: {
+        code: '12',
+        examName: 'обществознание',
+        date: '15.06.2021',
+      },
+      body: [
+        {
+          local_government_code: '111',
+          organization_code: '111002',
+          class: '11',
+          code_exam_point: '1018',
+          auditorium_number: '9',
+          last_name: 'Иванов',
+          first_name: 'Иван',
+          patronymic: 'Иванович',
+          short_answer: '++++++',
+          detailed_answer: '1(1)1(1)1(1)1(1)',
+          prymary_score: '6',
+          estimation: '39',
+        },
+      ],
+    }
+
+    const fileParser = new FileParser(head)
+    expect(fileParser.parseCsv()).toEqual(result)
+  })
+
+  test('Should return default rechecking', () => {
+    const rechecking = `;;;;;;;12 - Обществознание 2021.06.15;;;;;;;;;;;
+    №;Код ОО;Класс;Код ППЭ;Аудитория;Фамилия;Имя;;Отчество;Документ;Тип апелляции / перепроверки;Состояние;;Дата отправки на обработку;Дата изменений;Балл до изменения;Текущие;;Разница
+    ;;;;;;;;;;;;;;;;Первичный балл;Балл;
+    1;111002;11;1111;0002;Иванов;Иван;;Иванович;1111 222222;Апелляция на результат;Апелляция удовлетворена;;02.07.2021 16:55:40;05.07.2021 9:54:41;41;22;42;1
+    `
+
+    const result = {
+      isRecheking: true,
+      title: {
+        code: '12',
+        examName: 'обществознание',
+        date: '15.06.2021',
+      },
+      body: [
+        {
+          code_exam_point: '1111',
+          organization_code: '111002',
+          class: '11',
+          auditorium_number: '0002',
+          last_name: 'Иванов',
+          first_name: 'Иван',
+          patronymic: 'Иванович',
+          appeal_status: 'Апелляция удовлетворена',
+          appeal_type: 'Апелляция на результат',
+          send_for_processing_date: '02.07.2021 16:55:40',
+          change_date: '05.07.2021 9:54:41',
+          prymary_score: '22',
+          estimation: '42',
+        },
+      ],
+    }
+
+    const fileParser = new FileParser(rechecking)
+    expect(fileParser.parseCsv()).toEqual(result)
+  })
+
+  test("Should return error when don't have head in table", () => {
+    const head = `;;;;;;;12 - Обществознание 2021.06.15;;;;;;;;;;;
+    1;;111;;111002;;11;;1018;;9;;Иванов;Иван;Иванович;;1111;;222222;;++++++;;1(1)1(1)1(1)1(1);;6;;39;
+    `
+    const fileParser = new FileParser(head)
+    expect(() => fileParser.parseCsv()).toThrow("don't have head in table")
   })
 })
